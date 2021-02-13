8c47151bb1e88ae873b6978fac367bd9d8620035
Asim Hakami <asimhakami@hungerstation.com>
2021-02-13 13:53:12 +0300

    Initial Commit

---
+++ MovieDB_iOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieDB_iOS
+//
+//  Created by Asim Hakami on 01/07/1442 AH.
+//  Copyright © 1442 Asim Hakami. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
